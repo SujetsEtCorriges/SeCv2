4258699dd131cba4cdb96025100fb6c51dacf0e9
Mestiri Hedi <edfanhouse13@gmail.com>
2013-04-21 18:18:25 +0200

    correction bug buton switch

--- SetC : SujetsetCorriges/BacView.m
+++ SetC : SujetsetCorriges/BacView.m
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) IBOutlet UISwitch *optionSwitch1;
 @property (nonatomic, strong) IBOutlet UISwitch *optionSwitch2;
 @property (nonatomic, strong) IBOutlet UIScrollView *optionScroll1;
+@property (nonatomic, strong) IBOutlet UIScrollView *optionScroll2;
 
 @end
 
@@ -32,7 +33,6 @@
     UINib *nib = [UINib nibWithNibName:@"BacView" bundle:nil];
     UIView *myView = [[nib instantiateWithOwner:self options:nil] objectAtIndex:0];
     myView.frame=CGRectMake(0,yPosition,320,257);
-    [self hideScroll:_optionScroll1];
     self = (BacView *)myView;
     
     return self;
@@ -53,58 +53,66 @@
     
     if (optionSwitch.on)
     {
-        [UIView animateWithDuration: 1
-                              delay: 0
-                            options: (UIViewAnimationCurveEaseInOut)
-                         animations:^{
-                             optionSwitch.frame = CGRectMake(optionSwitch.frame.origin.x - 50, optionSwitch.frame.origin.y, optionSwitch.frame.size.height, optionSwitch.frame.size.width);
-                         }
-                         completion:^(BOOL finished) {
-                             [self displayScroll:_optionScroll1];
-                         }];
-        
+        _optionSwitch2.enabled = YES;
+        [self moveLeft:optionSwitch andDisplay:_optionScroll1];
     }
     else
     {
-        [self hideScroll:_optionScroll1];
-        [UIView animateWithDuration: 1
-                              delay: 0
-                            options: (UIViewAnimationCurveEaseInOut)
-                         animations:^{
-                             optionSwitch.frame = CGRectMake(optionSwitch.frame.origin.x + 50, optionSwitch.frame.origin.y, optionSwitch.frame.size.height, optionSwitch.frame.size.width);
-                         }
-                         completion:^(BOOL finished) {
-                             
-                         }];
+        _optionSwitch2.enabled = NO;
+        if (_optionSwitch2.on)
+        {
+            _optionSwitch2.on = NO;
+            [self moveRight:_optionSwitch2 andHide:_optionScroll2];
+        }
+        
+        [self moveRight:optionSwitch andHide:_optionScroll1];
     }
 }
 
 - (IBAction)optionSwitch2Changed:(id)sender
 {
     UISwitch *optionSwitch = (UISwitch *)sender;
-    
     if (optionSwitch.on)
     {
-        [UIView animateWithDuration: 1
-                              delay: 0
-                            options: (UIViewAnimationCurveEaseInOut)
-                         animations:^{
-                             optionSwitch.frame = CGRectMake(optionSwitch.frame.origin.x - 50, optionSwitch.frame.origin.y, optionSwitch.frame.size.height, optionSwitch.frame.size.width);
-                            
-                         } completion:nil];
+            [self moveLeft:optionSwitch andDisplay:_optionScroll2];            
     }
     else
     {
-        [UIView animateWithDuration: 1
-                              delay: 0
-                            options: (UIViewAnimationCurveEaseInOut)
-                         animations:^{
-                             optionSwitch.frame = CGRectMake(optionSwitch.frame.origin.x + 50, optionSwitch.frame.origin.y, optionSwitch.frame.size.height, optionSwitch.frame.size.width);
-                             
-                         } completion:nil];
+            [self moveRight:optionSwitch andHide:_optionScroll2];
     }
 }
 
+
+#pragma mark - mouvement method
+- (void)moveLeft:(UISwitch *)optionSwitch andDisplay:(UIScrollView *)scrollView
+{
+
+    [UIView animateWithDuration: 0.5
+                          delay: 0
+                        options: (UIViewAnimationCurveEaseInOut)
+                     animations:^{
+                         optionSwitch.frame = CGRectMake(125, optionSwitch.frame.origin.y, optionSwitch.frame.size.height, optionSwitch.frame.size.width);
+                     }
+                     completion:^(BOOL finished) {
+                         [self displayScroll:scrollView];
+                     }];
+}
+
+- (void)moveRight:(UISwitch *)optionSwitch andHide:(UIScrollView *)scrollView
+{
+    [self hideScroll:scrollView];
+    [UIView animateWithDuration: 0.5
+                          delay: 0
+                        options: (UIViewAnimationCurveEaseInOut)
+                     animations:^{
+                         optionSwitch.frame = CGRectMake(185, optionSwitch.frame.origin.y, optionSwitch.frame.size.height, optionSwitch.frame.size.width);
+                     }
+                     completion:^(BOOL finished) {
+                     }];
+}
+
+
+#pragma mark - scroll methods
 - (void)displayScroll:(UIScrollView *)scrollView
 {
     NSArray *array = [[NSArray alloc] initWithObjects:@"Latin/Grec", @"Autre", nil];
