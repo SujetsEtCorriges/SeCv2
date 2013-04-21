9e23f358dfc0dd2bfefcbd612bc1d237062073ae
Jérémy BENICHOU <jer.benichou@gmail.com>
2013-04-21 12:23:05 +0200

    Ajout des icônes sur les boutons de la page Détails

--- SetC : SujetsetCorriges/SujetsCorrigesDetailsViewController.m
+++ SetC : SujetsetCorriges/SujetsCorrigesDetailsViewController.m
@@ -57,12 +57,12 @@
     // Ajout des lignes dans la vue "détails"
     CALayer *lineBottom = [CALayer layer];
     lineBottom.backgroundColor = [UIColor colorWithWhite:0.8 alpha:1.0].CGColor;
-    lineBottom.frame = CGRectMake(10, 50, 280, 1);
+    lineBottom.frame = CGRectMake(10, 55, 280, 1);
     [viewDetails_.layer addSublayer:lineBottom];
     
     CALayer *lineBottom2 = [CALayer layer];
     lineBottom2.backgroundColor = [UIColor colorWithWhite:0.8 alpha:1.0].CGColor;
-    lineBottom2.frame = CGRectMake(10, 100, 280, 1);
+    lineBottom2.frame = CGRectMake(10, 110, 280, 1);
     [viewDetails_.layer addSublayer:lineBottom2];
     
     if(corrigePartiel_ == 1)
